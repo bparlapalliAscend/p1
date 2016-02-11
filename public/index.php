1cd3fef5b42577dff04aed973fc214143bc8f853
bparlapalliAscend <bharath.parlapalli@gmail.com>
2016-02-10 22:47:31 -0500

    prettified index.php with css

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
+
 <html>
 <head>
+<title>Bharath Parlapalli - DWA15 Portfolio</title>
 <link rel="stylesheet" type="text/css" href="css/base.css">
 </head>
 <body>
@@ -11,11 +13,32 @@
 		</div>
 
 		<div class="onethird">
-				<img src="imgs/bp.JPG" alt="residual self image" height="300px" width="460px">
+				<img src="imgs/bp.JPG" alt="residual self image">
 		</div>
 
 		<div class ="onethird">
-				<h3>Links go here</h3>
+			<div class="project">
+				<div class="projecttitle">Project 1</div>
+				<div class="psitelink"><a href="http://p1.bparlapalli.me">site link</a></div>
+				<div class="pgitlink"><a href="https://github.com/bparlapalliAscend/p1">github link</a></div>
+			</div>
+			<div class="project">
+				<div class="projecttitle">Project 2</div>
+				<div class="psitelink">site link</div>
+				<div class="pgitlink">github link</div>
+			</div>
+				<div class="project">
+				<div class="projecttitle">Project 3</div>
+				<div class="psitelink">site link</div>
+				<div class="pgitlink">github link</div>
+			</div>
+			
+			<div class="project">
+				<div class="projecttitle">Project 4</div>
+				<div class="psitelink">site link</div>
+				<div class="pgitlink">github link</div>
+			</div>
+			
 		</div>
 		
 		
